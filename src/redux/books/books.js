7517aab979fb7076b3f08b8b2953f3d6ab1742cc
187jjay187 <187jjay187@gmail.com>
2022-12-20 16:03:31 +0200

    Add actions and reducers to books.js in redux/books folder

---
+++ src/redux/books/books.js
@@ -0,0 +1,32 @@
+// const action types
+const LOAD_BOOKS = 'bookstore/books/LOAD_BOOKS';
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+
+// create actions for loadBooks, addBook, removeBook
+export const loadBooks = () => ({ type: LOAD_BOOKS });
+export const addBook = (book) => ({ type: ADD_BOOK, book });
+export const removeBook = (title) => ({ type: REMOVE_BOOK, title });
+
+// create reducer functions and cases to LOAD_BOOKS, ADD_BOOK, REMOVE_BOOK
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case LOAD_BOOKS:
+      return {
+        load: true,
+        books: [],
+      };
+    case ADD_BOOK:
+      return {
+        load: false,
+        books: [...state.books, action.book],
+      };
+    case REMOVE_BOOK:
+      return {
+        load: false,
+        books: state.books.filter((book) => book.title !== action.title),
+      };
+    default:
+      return state;
+  }
+}
