ca8fefb291b9763c6cf8d018ac66ad222e29e9ce
187jjay187 <187jjay187@gmail.com>
2022-12-21 18:38:58 +0200

    Add title and author value to AddBook.js

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
+
 // create add book component
 const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!title.trim() || !author.trim()) return;
+    dispatch(addBook({ id: uuidv4(), title, author }));
+    setTitle('');
+    setAuthor('');
   };
 
   // create useState and new handle function
@@ -11,11 +23,15 @@ const AddBook = () => {
         name="title"
         type="text"
         placeholder="book title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <input
         name="author"
         type="text"
         placeholder="author name"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
       />
       <button type="submit">Add Book</button>
     </form>
