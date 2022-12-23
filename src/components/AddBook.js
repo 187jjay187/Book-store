543fe549cbbf4cdf3dc573fcd8f4c334ffcad4a1
187jjay187 <187jjay187@gmail.com>
2022-12-23 15:16:15 +0200

    Connect Api to AddBook.js

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,17 +1,50 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { addBook } from '../redux/books/books';
+import { addBook, addBookError, reqAddBook } from '../redux/books/books';
 
 // create add book component
 const AddBook = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('action');
   const dispatch = useDispatch();
-  const handleSubmit = (e) => {
+  const { adding } = useSelector((state) => state.books, shallowEqual);
+  const sendBook = async () => {
+    dispatch(reqAddBook());
+    try {
+      const book = {
+        item_id: uuidv4(),
+        title,
+        author,
+        category,
+      };
+      const res = await fetch(process.env.REACT_APP_BOOKS, {
+        method: 'POST',
+        body: JSON.stringify(book),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (res.status === 201) {
+        dispatch(addBook(book));
+        setTitle('');
+        setAuthor('');
+        setCategory('');
+      } else if (res.status >= 400 && res.status < 500) {
+        dispatch(addBookError('You made a bad request!'));
+      } else {
+        dispatch(addBookError('There was a problem with the server!'));
+      }
+    } catch (err) {
+      dispatch(addBookError(err.message));
+    }
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (!title.trim() || !author.trim()) return;
-    dispatch(addBook({ id: uuidv4(), title, author }));
+    await sendBook();
     setTitle('');
     setAuthor('');
   };
@@ -33,7 +66,12 @@ const AddBook = () => {
         value={author}
         onChange={(e) => setAuthor(e.target.value)}
       />
-      <button type="submit">Add Book</button>
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="action">Action</option>
+        <option value="programming">Porgramming</option>
+        <option value="math">Math</option>
+      </select>
+      <button type="submit">{adding ? 'adding...' : 'Add Book'}</button>
     </form>
   );
 };
