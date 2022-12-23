b90d4988c49cb4068554336ef3b18fe2ce038a79
187jjay187 <187jjay187@gmail.com>
2022-12-23 15:17:06 +0200

    Connect API to Book.js file to remove books

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,39 @@
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { removeBook, reqRemoveBook, removeBookError } from '../redux/books/books';
 
 const Book = ({ id, title, author }) => {
   const dispatch = useDispatch();
+  const { selected } = useSelector((state) => state.books, shallowEqual);
+  // create function to removebooks
+  const handleClick = async () => {
+    dispatch(reqRemoveBook(id));
+    try {
+      const res = await fetch(`${process.env.REACT_APP_BOOKS}/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({ item_id: id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (res.status === 201) {
+        const data = await res.json();
+        dispatch(removeBook(data));
+      } else {
+        throw new Error(res.status);
+      }
+    } catch (error) {
+      dispatch(removeBookError(error.message));
+    }
+  };
   return (
     <div>
       <h2>{title}</h2>
       <h3>{author}</h3>
       <div>
-        <button type="button" onClick={() => dispatch(removeBook(id))}>Remove</button>
+        <button type="button" onClick={handleClick}>
+          {selected === id ? 'Removing...' : 'Remove'}
+        </button>
       </div>
     </div>
   );
